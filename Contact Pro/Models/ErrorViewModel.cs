f746c89dcb6a6e84bb79eb401f00b490bb99fd50
Brian Aponte <brianaponte78@gmail.com>
2024-02-14 22:30:59 -0500

    Add project files.

---
+++ Contact Pro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Contact_Pro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
